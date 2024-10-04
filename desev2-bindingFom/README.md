b951f918e7fb4b746c05ba6fa6dd0ed8f929a1d6
german-rs <ing.germanriveros@gmail.com>
2024-10-03 22:20:04 -0300

    Se realizan mejoras extras

--- desev2-bindingFom/README.md
+++ desev2-bindingFom/README.md
@@ -1,2 +1,17 @@
-# Desafío - Binding de formularios (I)
+# Desafío - Binding de Formularios (I)
+
+En este desafío se pondrán a prueba los conocimientos sobre **data binding** en Vue.js utilizando directivas. El objetivo es crear una aplicación que permita el llenado dinámico de un formulario para los datos de una tarjeta de crédito.
+
+## Descripción del Desafío
+
+Se deberá crear una aplicación en **Vue.js** que permita:
+- Generar un formulario interactivo que actualice en tiempo real los datos de una tarjeta de crédito.
+- Implementar **data binding** para que los cambios en los inputs se reflejen directamente en la vista de la tarjeta.
+- adicionalmente se modularizo el código de CSS y se agregaron tooltip para facilitar el llenado de los campos.
+
+Este desafío es ideal para reforzar las habilidades en Vue.js y profundizar en el manejo de formularios dinámicos.
+
+### Resultado final
+
+![Un formulario junto una tarjeta de crédito color negro](./public/deseva2.png)
 
--- desev2-bindingFom/src/App.vue
+++ desev2-bindingFom/src/App.vue
@@ -1,3 +1,10 @@
+<!--
+    Nombre estudiante: Germán A. Riveros Sepúlveda.
+    Generación: 14.
+    Jornada: Vespertina.   
+    Desafío evaluado: Binding de formularios (I)
+-->
+
 <script>
 export default {
   data() {
@@ -7,9 +14,15 @@ export default {
       numeroTarjeta: '',
       fechaExpiracion: '',
       propietario: '',
-      propietario: '',
       rutaImagenVisa: ''
     }
+  },
+   mounted() {
+    // Inicializar los tooltips de Bootstrap 5.3 manualmente
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => {
+      return new bootstrap.Tooltip(tooltipTriggerEl);
+    });
   }
 }
 </script>
@@ -18,47 +31,53 @@ export default {
   <div id="app">
       <form>
         <div>
-          <label>Título de la tarjeta: </label> <!-- Visa Classic Crédito -->
-          <input v-model="tituloTarjeta"/>
+          <label>Título de la tarjeta: </label>
+          <input v-model="tituloTarjeta" data-bs-toggle="tooltip" data-bs-placement="top"
+          data-bs-title="Visa Classic Crédito"/>
         </div>
 
         <div>
           <label>Chip SRC: </label>
-          <input v-model="rutaImagenChip"/> <!-- /chip.png -->
+          <input v-model="rutaImagenChip" data-bs-toggle="tooltip" data-bs-placement="top"
+          data-bs-title="/chip.png"/> 
         </div>
         
         <div>
           <label>Número: </label>
-          <input v-model="numeroTarjeta"/> <!-- 400 500 600 700 -->
+          <input v-model="numeroTarjeta" data-bs-toggle="tooltip" data-bs-placement="top"
+          data-bs-title="400 500 600 700"/>
         </div>
         
         <div>
           <label>Fecha de expiración: </label>
-          <input v-model="fechaExpiracion"/> <!-- 01/30 -->
+          <input v-model="fechaExpiracion" data-bs-toggle="tooltip" data-bs-placement="top"
+          data-bs-title="01/30"/>
         </div>
         
         <div>
           <label>Propietario: </label>
-          <input v-model="propietario"/> <!-- WILLIAM HENRY GATES III -->
+          <input v-model="propietario" data-bs-toggle="tooltip" data-bs-placement="top"
+          data-bs-title="WILLIAM HENRY GATES III"/>
         </div>
         
         <div>
           <label>Tipo de tarjeta SRC: </label>
-          <input v-model="rutaImagenVisa"/> <!-- /visa.png -->
+          <input v-model="rutaImagenVisa" data-bs-toggle="tooltip" data-bs-placement="top"
+          data-bs-title="/visa.png"/>
         </div>
       </form>
 
       <div class="carnet">
         <h3>{{ tituloTarjeta }}</h3>
         
-        <img width="40" :src="rutaImagenChip" alt="" />
+        <img width="40" :src="rutaImagenChip" alt="imagen de un chip" />
         <div>
           <h2>{{ numeroTarjeta }}</h2>
           <span>Fecha Exp: <b>{{ fechaExpiracion }}</b></span>
         </div>
         <footer>
           <span>{{ propietario}}</span>
-          <img :src="rutaImagenVisa" width="60" />
+          <img :src="rutaImagenVisa" width="60" alt="logo de visa"/>
         </footer>
       </div>
     </div>
@@ -66,80 +85,32 @@ export default {
 </template>
 
 <style scoped>
-* {
-  margin: 0;
+.carnet {
+  width: 400px;
+  height: 230px;
+  background: var(--clr-fondo-tarjeta);
+  color: white;
+  border-radius: 20px;
+  padding: 20px;
+  box-shadow: 0px 0px 10px 2px var(--clr-negro);
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 }
-      body {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        height: 60vh;
-        font-size: 10px;
-        font-family: sans-serif;
-      }
 
-      #app {
-        display: flex;
-        gap: 40px;
-      }
-      form {
-        padding: 18px;
-        border-radius: 20px;
-        border: ridge 1px;
-        box-shadow: 1px 1px 1px 1px gray;
-        font-weight: bold;
-        display: flex;
-        flex-direction: column;
-        gap: 15px;
-        justify-content: center;
-      }
-      form div {
-        display: grid;
-        grid-template-columns: 100px 200px;
-        align-items: center;
-        gap: 5px;
-      }
+h3 {
+  font-size: 20px;
+}
 
-      form div label {
-        text-align: right;
-      }
+h2 {
+  font-size: 30px;
+  letter-spacing: 10px;
+  margin-bottom: 15px;
+  text-shadow: 0px 0px 1px gold;
+  font-family: Times;
+}
 
-      .carnet {
-        width: 400px;
-        height: 230px;
-        background: #1c1a1e;
-        color: white;
-        border-radius: 20px;
-        padding: 20px;
-        box-shadow: 0px 0px 10px 2px black;
-        display: flex;
-        flex-direction: column;
-        justify-content: space-between;
-      }
-
-      h3 {
-        font-size: 20px;
-      }
-
-      h2 {
-        font-size: 30px;
-        letter-spacing: 10px;
-        margin-bottom: 15px;
-        text-shadow: 0px 0px 1px gold;
-        font-family: Times;
-      }
-
-      b {
-        font-size: 16px;
-      }
-
-      footer {
-        display: flex;
-        justify-content: space-between;
-        align-items: flex-end;
-        font-weight: bold;
-        font-size: 18px;
-        text-transform: uppercase;
-        font-style: italic;
-      }
+b {
+  font-size: 16px;
+}
 </style>
